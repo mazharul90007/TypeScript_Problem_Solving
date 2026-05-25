4a8de766cd151a8c88099939742a0d11f8939fc9
mazharul90007 <mazharul90007@gmail.com>
2026-05-25 16:42:13 +0600

    Reverse a String problem solved

---
+++ day1.ts
@@ -0,0 +1,16 @@
+// Number1. Reverse a string
+// Write a function reverseString(str) that takes a string and returns it reversed.
+
+//=========JavaScript Split (String)==========
+const myName: string = "My name is Mazharul Islam Sourabh";
+const result = myName.split(" ");
+
+// console.log(result);
+
+// ========JavaScript Reverse (Array)==========
+const resultReverse = result.reverse();
+// console.log(resultReverse);
+
+//========JavaScript Join (Array)==========
+const joinReverseResult = resultReverse.join(" ");
+console.log(joinReverseResult);
